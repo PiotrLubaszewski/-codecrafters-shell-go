17641065b9dbe5715aebc2857b07f1492d21c7a8
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2025-03-09 21:01:54 +0100

    Attempt 1: The type builtin: buildins

--- app/main.go
+++ app/main.go
@@ -12,6 +12,7 @@ import (
 var commands = map[string]func(args []string){
 	"exit": handleExit,
 	"echo": handleEcho,
+	"type": handleType,
 }
 
 // Terminates with code/status 0.
@@ -33,6 +34,21 @@ func handleEcho(args []string) {
 	msg := strings.Join(args, " ")
 	fmt.Fprintln(os.Stdout, msg)
 }
+
+// Type command check for builtin commands and unrecognized commands 
+func handleType(args []string) { 
+	command := args[0]
+
+	msg := ""
+
+	if checkCommand(command) {
+		msg = fmt.Sprintf("%s is a shell bulidin", command)
+	} else {
+		msg = fmt.Sprintf("%s: not found ", command)
+	}
+	
+	fmt.Fprintln(os.Stdout, msg)
+} 
 
 // Reads and returns inserted user commands
 func readCommandAndArgs() (string, []string, error) {
@@ -56,6 +72,11 @@ func executeCommand(command string, args []string) {
 	} else {
 		fmt.Printf("%s: command not found\n", command)
 	}
+}
+
+func checkCommand(command string) bool {
+	_, exists := commands[command]
+	return exists
 }
 
 func main() {
