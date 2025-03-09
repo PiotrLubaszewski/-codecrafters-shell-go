7e81d0489bc29fcc53811e5f4eac97db831c3a8b
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2025-03-09 16:16:13 +0100

    The exit buitin

--- app/main.go
+++ app/main.go
@@ -9,17 +9,38 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+// Command and their actions
+var commands = map[string]func()  {
+	"exit 0": func () { os.Exit(0) }, 
+}
+
+// Reads and returns inserted user commands
+func readCommand() (string, error) {
+	fmt.Fprint(os.Stdout, "$ ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		return "", scanner.Err()
+	}
+	return strings.TrimSpace(scanner.Text()), nil
+}
+
+// If command exists, executes command, else returns error
+func executeCommand(command string) {
+	if action, exists := commands[command]; exists {
+		action()
+	} else {
+		fmt.Printf("%s: command not found\n", command)
+	}
+}
+
 func main() {
 	// Uncomment this block to pass the first stag
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		command, err := readCommand()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		executeCommand(command)
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
-	main()
 }
