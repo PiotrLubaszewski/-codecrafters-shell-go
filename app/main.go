b731667366c941eb9e21f441e9a6fc4937a94a64
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2025-03-09 20:48:44 +0100

    Attempt 2: The echo builtin

--- app/main.go
+++ app/main.go
@@ -17,7 +17,7 @@ var commands = map[string]func(args []string)  {
 }
 
 // Terminates with code/status 0.
-func handleExit(args) {
+func handleExit(args string[]) {
 	if len(args) > 1 {
 		exit_code, err := strconv.Atoi(args[1])
 		if err != nil {
@@ -30,9 +30,9 @@ func handleExit(args) {
 }
 
 // Echo command prints the provided text back.
-func handleEcho(input string) {
-	msg := strings.Join(input, " ")
-	fmt.Fprintln(os.Std, msg)
+func handleEcho(args []string) {
+	msg := strings.Join(args, " ")
+	fmt.Fprintln(os.Stdout, msg)
 }
 
 // Reads and returns inserted user commands
