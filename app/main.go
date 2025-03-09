9939dd77bab4b392a8a68a450f884468ce9e6517
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2025-03-09 22:11:24 +0100

    Attempt 3: The pwd builtin

--- app/main.go
+++ app/main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -16,61 +15,90 @@ type CommandRegistry struct {
 	commands map[string]func([]string)
 }
 
-// NewCommandRegistry tworzy nowy rejestr komend
+// Nowy rejestr komend
 func NewCommandRegistry() *CommandRegistry {
 	return &CommandRegistry{commands: make(map[string]func([]string))}
 }
 
-// Register dodaje komendę do rejestru
+// Inicjalizacja rejestru komend
+func InitializeRegistry() *CommandRegistry {
+	registry := NewCommandRegistry()
+	registry.Register("exit", handleExit)
+	registry.Register("echo", handleEcho)
+	registry.Register("type", func(args []string) { handleType(args, registry) })
+	registry.Register("pwd", handlePwd)
+	return registry
+}
+
+// Rejestracja nowej komendy
 func (cr *CommandRegistry) Register(name string, handler func([]string)) {
 	cr.commands[name] = handler
 }
 
-// Execute wykonuje komendę z rejestru lub jako polecenie zewnętrzne
+// Wykonanie komendy
 func (cr *CommandRegistry) Execute(command string, args []string) {
 	if action, exists := cr.commands[command]; exists {
 		action(args)
+	}  else if out, err := exec.Command(command, args...).Output(); err == nil {
+		fmt.Print(string(out))
 	} else {
-		cr.executeExternal(command, args)
+		fmt.Printf("%s: command not found\n", command)
 	}
 }
 
-// executeExternal wykonuje komendę zewnętrzną, jeśli nie jest w rejestrze
-func (cr *CommandRegistry) executeExternal(command string, args []string) {
-	out, err := exec.Command(command, args...).Output()
-	if err != nil {
-		fmt.Printf("%s: command not found\n", command)
+// Sprawdzenie, czy komenda istnieje
+func (cr *CommandRegistry) Exists(command string) bool {
+	_, exists := cr.commands[command]
+	return exists
+}
+
+// Obsługuje wyjście z programu
+func handleExit(args []string) {
+	exitCode := 0
+	if len(args) > 0 {
+		var err error
+		exitCode, err = strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("Invalid exit code")
+			return
+		}
+	}
+	os.Exit(exitCode)
+}
+
+// Obsługuje polecenie echo
+func handleEcho(args []string) {
+	fmt.Println(strings.Join(args, " "))
+}
+
+// Obsługuje polecenie type
+func handleType(args []string, registry *CommandRegistry) {
+	if len(args) == 0 {
+		fmt.Println("type: missing argument")
 		return
 	}
-	fmt.Print(string(out))
+
+	command := args[0]
+	if registry.Exists(command) {
+		fmt.Printf("%s is a shell builtin\n", command)
+	} else if path, err := exec.LookPath(command); err == nil {
+		fmt.Printf("%s is %s\n", command, path)
+	} else {
+		fmt.Printf("%s: not found\n", command)
+	}
 }
 
-// Shell reprezentuje powłokę z rejestrem komend
+// Zarządza interakcją z użytkownikiem
 type Shell struct {
 	registry *CommandRegistry
 }
 
-// NewShell tworzy nową instancję powłoki
+// Nowa instancja shella
 func NewShell(registry *CommandRegistry) *Shell {
 	return &Shell{registry: registry}
 }
 
-// Run uruchamia pętlę shella
-func (s *Shell) Run() {
-	for {
-		command, args, err := s.readCommandAndArgs()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
-		}
-		if command == "" {
-			continue
-		}
-		s.registry.Execute(command, args)
-	}
-}
-
-// readCommandAndArgs wczytuje i zwraca komendę użytkownika
+// Wczytuje i zwraca komendę użytkownika
 func (s *Shell) readCommandAndArgs() (string, []string, error) {
 	fmt.Print("$ ")
 	scanner := bufio.NewScanner(os.Stdin)
@@ -91,59 +119,7 @@ func (s *Shell) readCommandAndArgs() (string, []string, error) {
 	return splitted[0], splitted[1:], nil
 }
 
-// handleExit obsługuje wyjście z programu
-func handleExit(args []string) {
-	exitCode := 0
-	if len(args) > 0 {
-		var err error
-		exitCode, err = strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println("Invalid exit code")
-			return
-		}
-	}
-	os.Exit(exitCode)
-}
-
-// handleEcho obsługuje polecenie echo
-func handleEcho(args []string) {
-	fmt.Println(strings.Join(args, " "))
-}
-
-// handleType obsługuje polecenie type
-func handleType(args []string) {
-	if len(args) == 0 {
-		fmt.Println("type: missing argument")
-		return
-	}
-
-	command := args[0]
-
-	// Sprawdzanie, czy komenda jest wbudowana
-	if isBuiltin(command) {
-		fmt.Printf("%s is a shell builtin\n", command)
-	} else {
-		// Sprawdzanie, czy komenda jest dostępna w systemie
-		if path, err := exec.LookPath(command); err == nil {
-			fmt.Printf("%s is %s\n", command, path)
-		} else {
-			fmt.Printf("%s: not found\n", command)
-		}
-	}
-}
-
-// isBuiltin sprawdza, czy komenda jest wbudowaną komendą powłoki
-func isBuiltin(command string) bool {
-	builtinCommands := []string{"exit", "echo", "type", "pwd"} // Można dodać więcej komend wbudowanych
-	for _, builtin := range builtinCommands {
-		if command == builtin {
-			return true
-		}
-	}
-	return false
-}
-
-// handlePwd obsługuje polecenie pwd
+// Wypisuje ścieżkę 
 func handlePwd(args []string) {
 	ex, err := os.Executable()
     if err != nil {
@@ -153,14 +129,19 @@ func handlePwd(args []string) {
     fmt.Println(exPath)
 }
 
-// InitializeRegistry tworzy i inicjalizuje rejestr komend
-func InitializeRegistry() *CommandRegistry {
-	registry := NewCommandRegistry()
-	registry.Register("exit", handleExit)
-	registry.Register("echo", handleEcho)
-	registry.Register("type", handleType)
-	registry.Register("pwd", handlePwd)
-	return registry
+// Uruchamia pętlę shella
+func (s *Shell) Run() {
+	for {
+		command, args, err := s.readCommandAndArgs()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		if command == "" {
+			continue
+		}
+		s.registry.Execute(command, args)
+	}
 }
 
 func main() {
